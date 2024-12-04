9ade56cc8d88cb813db711d95d04ee1d48c13e95
torohkiss <torohkiss@yahoo.com>
2024-12-04 16:27:29 +0000

    task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- script that creates a view need_meeting
+-- Drop the view if it already exists
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
