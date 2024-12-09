2c4e158cdb53e6cf1c3a4feff9b310f3fe16ede7
torohkiss <torohkiss@yahoo.com>
2024-12-09 02:00:06 +0000

    task 9a

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,6 +5,5 @@
 def insert_school(mongo_collection, **kwargs):
     """Python function that inserts new document in a collection"""
 
-    for key, value in kwargs.items():
-        m = mongo_collection.insert_one({key: value})
-        return m.inserted_id
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
