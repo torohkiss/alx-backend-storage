0ed0fbf19cfc9c31cd4f3fbe72bb1542209a63f3
torohkiss <torohkiss@yahoo.com>
2024-12-10 18:56:48 +0000

    task0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+
+
+import redis
+import uuid
+
+
+class Cache:
+    """A Cache class that will write strings to redis"""
+
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: [str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
