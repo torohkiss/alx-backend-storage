8d7ea3d98d72b1efbafae593784fb6edeeeb4a3d
torohkiss <torohkiss@yahoo.com>
2025-05-18 14:47:27 +0100

    task 1a

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,8 @@
 -- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
--- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- country, enumeration of countries: US, CO and TN, never null
+-- (= default will be the first element of the enumeration, here US)
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
 
